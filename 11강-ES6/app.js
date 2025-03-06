87cda306bc6e136f6f1d07b7b3a431fc505d55df
PepperShin <peep1996@naver.com>
2025-03-06 15:09:33 +0900

    2025-03-06

---
+++ 11강-ES6/app.js
@@ -0,0 +1,5 @@
+import { baseData, clean } from './utility.js';
+
+import person from './person.js'
+//또는
+import './person.js'
---
+++ 11강-ES6/person.js
@@ -0,0 +1,6 @@
+const person = {
+    name : "홍길동"
+}
+
+// default는 한 파일에 하나만 할당 가능.
+export default person;
